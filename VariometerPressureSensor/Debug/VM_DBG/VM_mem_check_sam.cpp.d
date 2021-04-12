a53437bc444ec375c0fc8a45cd0cfd0a024d76de
oiver12 <36069780+oiver12@users.noreply.github.com>
2021-04-12 19:59:52 +0200

    Revert "Initial commit"
    This reverts commit 84037f453c121c22b007c021df90d54dfb30e52c.

--- VariometerPressureSensor/Debug/VM_DBG/VM_mem_check_sam.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Oliver\AppData\Local\Temp\VMBuilds\VariometerPressureSensor\nano_atmega328old\Debug\VM_DBG\VM_mem_check_sam.cpp.o: \
- c:\program\ files\ (x86)\microsoft\ visual\ studio\2017\community\common7\ide\extensions\2kamu2fe.kiw\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
--- VariometerPressureSensor/Debug/Wire/Wire.cpp.d
+++
@@ -1,8 +0,0 @@
-C:\Users\Oliver\AppData\Local\Temp\VMBuilds\VariometerPressureSensor\nano_atmega328old\Debug\Wire\Wire.cpp.o: \
- D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\libraries\Wire\src\Wire.cpp \
- D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\libraries\Wire\src\utility/twi.h \
- D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\libraries\Wire\src\Wire.h \
- D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\cores\arduino/Stream.h \
- D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\cores\arduino/Print.h \
- D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\cores\arduino/WString.h \
- D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\cores\arduino/Printable.h
