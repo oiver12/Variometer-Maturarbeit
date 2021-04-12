84037f453c121c22b007c021df90d54dfb30e52c
oiver12 <36069780+oiver12@users.noreply.github.com>
2021-04-12 19:58:50 +0200

    Initial commit

---
+++ VariometerPressureSensor/Release/SPI/SPI.cpp.d
@@ -0,0 +1,15 @@
+C:\Users\Oliver\AppData\Local\Temp\VMBuilds\VariometerPressureSensor\nano_atmega328\Release\SPI\SPI.cpp.o: \
+ D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\libraries\SPI\src\SPI.cpp \
+ D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\libraries\SPI\src\SPI.h \
+ D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\cores\arduino/Arduino.h \
+ D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\cores\arduino/binary.h \
+ D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\cores\arduino/WString.h \
+ D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\cores\arduino/Stream.h \
+ D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\cores\arduino/Print.h \
+ D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\cores\arduino/Printable.h \
+ D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\cores\arduino/Arduino.h \
+ D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\variants\eightanaloginputs/pins_arduino.h \
+ d:\programme\arduino\arduino-1.8.13\hardware\arduino\avr\variants\standard\pins_arduino.h
---
+++ VariometerPressureSensor/Release/Wire/utility/twi.c.d
@@ -0,0 +1,7 @@
+C:\Users\Oliver\AppData\Local\Temp\VMBuilds\VariometerPressureSensor\nano_atmega328\Release\Wire\utility\twi.c.o: \
+ D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\libraries\Wire\src\utility\twi.c \
+ D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\cores\arduino/Arduino.h \
+ D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\cores\arduino/binary.h \
+ D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\variants\eightanaloginputs/pins_arduino.h \
+ d:\programme\arduino\arduino-1.8.13\hardware\arduino\avr\variants\standard\pins_arduino.h \
+ D:\Programme\Arduino\arduino-1.8.13\hardware\arduino\avr\libraries\Wire\src\utility\twi.h
